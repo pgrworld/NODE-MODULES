45a4ec59f3c109d66a3bdcd5f4eb53f541d0bdf1
pgrworld <51364621+pgrworld@users.noreply.github.com>
2019-09-05 11:52:28 +0530

    Update http  module.js

--- http  module.js
+++ http  module.js
@@ -1,13 +1,12 @@
-
-//.1 HHTP MODULE
+//.1 http  module
 var http = require("http");
  http.createServer(function(req,res){
+ 	res.writeHead(200, {'Content-Type':'text/html'})
  	res.write('<b>securifi<b> <br>');
  	res.write("secrifi embedded systems")
  	res.write(req.url)              
  	res.end()
  }).listen(4000);
 console.log("Running port no:4000")
-
 
 
