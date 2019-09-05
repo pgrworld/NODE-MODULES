46596d95b166a10c07b5a0ca1a3ea50809dd264b
pgrworld <51364621+pgrworld@users.noreply.github.com>
2019-09-05 12:30:47 +0530

    Create url module.js

---
+++ url module.js
@@ -0,0 +1,7 @@
+//.2 URL MODULE(Split a web address into readable parts)
+var url = require("url");
+var adr = "https://www.w3schools.com/nodejs/nodejs_url.asp"
+var q = url.parse(adr);
+
+console.log(q.host)
+console.log(q.pathname)
